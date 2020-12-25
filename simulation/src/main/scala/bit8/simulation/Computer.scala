f2e8e3a3b267732d837ae6ebf05ea12232cdebca
Ovidijus <ovidijus.bickus@viavisolutions.com>
2020-12-25 13:31:20 +0200

    Simplify output watcher

--- simulation/src/main/scala/bit8/simulation/Computer.scala
+++ simulation/src/main/scala/bit8/simulation/Computer.scala
@@ -7,8 +7,6 @@ import bit8.simulation.modules.{ClockModule, OutputWatcher}
 import bit8.simulation.components.utils.IntegerWithOverflow._
 import bit8.simulation.components.utils.Utils.IntOverflowUtils
 
-import scala.concurrent.duration.DurationLong
-import scala.concurrent.Await
 import scala.language.postfixOps
 
 object Computer {
@@ -20,16 +18,17 @@ object Computer {
     val (busValueIn, busValueOut) = Cable()
     val inputCable = Cable()
 
+    overflowInt[Bit8](input).setConn(inputCable._1)
+
     val program = Compiler.compile(code).zipWithIndex.map(t => t._2 -> t._1).toMap
     val microCode = MicroCompiler.compile(Instruction.fullInstructionSet(MicroCompiler.maxInstructions()))
 
     val clock = new ClockModule(0, clk.left, outEnabled.right, outputRead.left, halt.right)
     val computer = new Cpu(clk.right, outEnabled.left, halt.left, busValueIn, inputCable._2, program, microCode(0), microCode(1), microCode(2), microCode(3))
-    overflowInt[Bit8](input).setConn(inputCable._1)
-    val output = OutputWatcher(clk.right, halt.right, outEnabled.right, outputRead.right, busValueOut)
+    val output = new OutputWatcher(clk.right, outEnabled.right, outputRead.right, busValueOut)
 
     clock.start()
-    Await.result(output, 10000 seconds)
+    output.getResult()
   }
 
   def run(code: String*): Int = run(code.toList)
